4c21df7046c1df2364e6a029ae6b1ac2cedf90e5
Saurabh Trivedi <trivedi@qti.qualcomm.com>
2018-09-27 12:39:38 -0700

    mail2

--- mail.php
+++ mail.php
@@ -1,1 +1,10 @@
-Hi there!
+<?php
+// the message
+$msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("someone@example.com","My subject",$msg);
+?>
